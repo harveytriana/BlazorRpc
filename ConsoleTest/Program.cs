7eb58e6411b81f013fc001a77bbb5d5239905433
harveytriana <harveytriana@hotmail.com>
2022-03-10 20:58:52 -0500

    2058

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -1,31 +1,58 @@
 ﻿
 using Grpc.Net.Client;
 using BlazorRpc;
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.DependencyInjection;
 
-Console.WriteLine("Hello, Blazor GRPC!");
+Console.WriteLine("Hello, RPC!");
 
 Thread.Sleep(300);
 
 var serviceUrl = "https://localhost:7158/";
 
 Console.WriteLine(serviceUrl);
-Console.WriteLine("Pres any key when rever is ready");
-Console.ReadLine();
-
-try {
-    using var channel = GrpcChannel.ForAddress(serviceUrl);
-
-    var rf = new RF.RFClient(channel);
-
-    var legs = new LegsRequest {
-        A = 1.0,
-        B = 1.0
-    };
-    var hypotenuse = rf.Hypotenuse(legs);
-
-    Console.WriteLine("Hypotenus({0},{1}) = {2}", legs.A, legs.B, hypotenuse.Y);
-}
-catch(Exception exception) {
-    Console.WriteLine("Exception:\n" + exception.Message);
-}
+Console.WriteLine("Press any key when server is ready\n");
 Console.ReadKey();
+
+// parameter
+var legs = new LegsRequest {
+    A = 1.0,
+    B = 1.0
+};
+
+GrpcTest().Wait();
+SignalRTest().Wait();
+
+Console.WriteLine("Bye");
+Console.ReadKey();
+
+async Task GrpcTest()
+{
+    try {
+        using var channel = GrpcChannel.ForAddress(serviceUrl);
+
+        var rf = new RF.RFClient(channel);
+
+        var hypotenuse = await rf.HypotenuseAsync(legs);
+
+        Console.WriteLine("From GRPC\tHypotenus({0},{1}) = {2}", legs.A, legs.B, hypotenuse.Y);
+    }
+    catch { }
+}
+
+
+async Task SignalRTest()
+{
+    try {
+        var _connection = new HubConnectionBuilder()
+               .WithUrl(serviceUrl + "RpcFunctionsHub")
+               .AddMessagePackProtocol() // binary protocol
+               .Build();
+        await _connection.StartAsync();
+
+        var hypotenuse = await _connection.InvokeAsync<double>("Hypotenuse", legs);
+
+        Console.WriteLine("From SignalR\tHypotenus({0},{1}) = {2}", legs.A, legs.B, hypotenuse);
+    }
+    catch { }
+}
