aaeb08b62bc5935b5755f1b0c0d08a161a6c6bad
Luis Harvey Triana Vega <harveytriana@hotmail.com>
2024-02-23 10:42:27 -0500

    Tests

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -1,6 +1,8 @@
 ﻿
 using BlazorRpc;
+
 using Grpc.Net.Client;
+
 using Microsoft.AspNetCore.SignalR.Client;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -8,10 +10,14 @@ Console.WriteLine("Hello, RPC!");
 
 var serviceUrl = "https://localhost:7158/"; // blazor server
 
-Console.WriteLine(serviceUrl);
-Console.WriteLine("Press any key when server is ready\n");
-Console.ReadKey();
+// var serviceUrl = "https://localhost:7002/"; // blazor server alone
+// var serviceUrl = "http://localhost:3008/"; // blazor server alone (Docker test)
 
+Console.WriteLine(serviceUrl);
+if(serviceUrl.Contains("7158")) {
+    Console.WriteLine("Runs multiple projects in Visual Studio. Press any key when server is ready\n");
+    Console.ReadKey();
+}
 // parameter
 var legs = new LegsRequest {
     A = 1.0,
@@ -34,7 +40,9 @@ async Task GrpcTest()
 
         Console.WriteLine("From GRPC\tHypotenuse({0},{1}) = {2}", legs.A, legs.B, hypotenuse.Y);
     }
-    catch { }
+    catch(Exception exception) {
+        Console.WriteLine("GrpcTest exception\n{0}\n", exception.Message);
+    }
 }
 
 async Task SignalRTest()
@@ -50,5 +58,7 @@ async Task SignalRTest()
 
         Console.WriteLine("From SignalR\tHypotenuse({0},{1}) = {2}", legs.A, legs.B, hypotenuse);
     }
-    catch { }
+    catch(Exception exception) {
+        Console.WriteLine("SignalRTest exception\n{0}\n", exception.Message);
+    }
 }
